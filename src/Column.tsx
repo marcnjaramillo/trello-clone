0f4523d924f949177b1b25c83073721788de3903
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-05-25 14:02:08 -0700

    Create Column component

---
+++ src/Column.tsx
@@ -0,0 +1,17 @@
+import React, { PropsWithChildren } from 'react';
+import { ColumnContainer, ColumnTitle } from './styles';
+import { AddNewItem } from './AddNewItem';
+
+interface ColumnProps {
+  text: string
+}
+
+export const Column = ({ text, children }: PropsWithChildren<ColumnProps>) => {
+  return (
+    <ColumnContainer>
+      <ColumnTitle>{text}</ColumnTitle>
+      {children}
+      <AddNewItem toggleButtonText='+ Add another task' onAdd={console.log} dark />
+    </ColumnContainer>
+  )
+}
