4ba65a6585ed797ac4b3851c225ca2cd454fcc22
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-05-25 17:46:32 -0700

    Create AppStateContext for state management

---
+++ src/AppStateContext.tsx
@@ -0,0 +1,95 @@
+import React, { createContext, useReducer, useContext, PropsWithChildren } from 'react';
+import { v4 as uuid } from 'uuid';
+import { findItemIndexById } from './utils/findItemIndexById';
+
+interface Task {
+  id: string
+  text: string
+}
+
+interface List {
+  id: string
+  text: string
+  tasks: Task[]
+}
+
+interface AppStateContextProps {
+  state: AppState
+  dispatch: React.Dispatch<any>
+}
+
+const appData: AppState = {
+  lists: [
+    {
+      id: '0',
+      text: 'To Do',
+      tasks: [{ id: 'c0', text: 'Generate app scaffold' }]
+    },
+    {
+      id: '1',
+      text: 'In Progress',
+      tasks: [{ id: 'c2', text: 'Learn Typescript' }]
+    },
+    {
+      id: '2',
+      text: 'Done',
+      tasks: [{ id: 'c3', text: 'Begin to use static typing' }]
+    }
+  ]
+}
+
+const AppStateContext = createContext<AppStateContextProps>({} as AppStateContextProps)
+
+type Action =
+  | {
+    type: 'ADD_LIST'
+    payload: string
+  }
+  | {
+    type: 'ADD_TASK'
+    payload: { text: string; taskId: string }
+  }
+
+const appStateReducer = (state: AppState, action: Action): AppState => {
+  switch (action.type) {
+    case 'ADD_LIST': {
+      return {
+        ...state,
+        lists: [
+          ...state.lists,
+          { id: uuid(), text: action.payload, tasks: [] }
+        ]
+      }
+    }
+    case 'ADD_TASK': {
+      const targetLaneIndex = findItemIndexById(state.lists, action.payload.taskId)
+
+      state.lists[targetLaneIndex].tasks.push({
+        id: uuid(),
+        text: action.payload.text
+      })
+
+      return {
+        ...state
+      }
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+export interface AppState {
+  lists: List[]
+}
+
+export const AppStateProvider = ({ children }: PropsWithChildren<{}>) => {
+  const [state, dispatch] = useReducer(appStateReducer, appData)
+  return (
+    <AppStateContext.Provider value={{ state, dispatch }}>{children}</AppStateContext.Provider>
+  )
+}
+
+export const useAppState = () => {
+  return useContext(AppStateContext)
+}
