b5972bf55356cf329a2394176d3e337cf3ca9e23
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-05-25 17:48:39 -0700

    Add state and action for creating new tasks

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,17 +1,29 @@
-import React, { PropsWithChildren } from 'react';
-import { ColumnContainer, ColumnTitle } from './styles';
+import React from 'react';
 import { AddNewItem } from './AddNewItem';
+import { useAppState } from './AppStateContext';
+import { Card } from './Card';
+import { ColumnContainer, ColumnTitle } from './styles';
 
 interface ColumnProps {
   text: string
+  index: number
+  id: string
 }
 
-export const Column = ({ text, children }: PropsWithChildren<ColumnProps>) => {
+export const Column = ({ text, index, id }: ColumnProps) => {
+  const { state, dispatch } = useAppState();
+
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      {children}
-      <AddNewItem toggleButtonText='+ Add another task' onAdd={console.log} dark />
+      {state.lists[index].tasks.map((task, i) => (
+        <Card text={task.text} key={task.id} index={i} />
+      ))}
+      <AddNewItem
+        toggleButtonText='+ Add another task'
+        onAdd={text => dispatch({ type: 'ADD_TASK', payload: { text, taskId: id } })}
+        dark
+      />
     </ColumnContainer>
   )
 }
