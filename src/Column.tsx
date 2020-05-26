40e6815efebded6ccd446de3751d60f3f362fcc5
marcnjaramillo <marcn.jaramillo@gmail.com>
2020-05-26 00:32:20 -0700

    Add drag and drop functionality for columns

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useDrop } from 'react-dnd';
+
 import { AddNewItem } from './AddNewItem';
 import { useAppState } from './AppStateContext';
 import { Card } from './Card';
 import { ColumnContainer, ColumnTitle } from './styles';
+import { useItemDrag } from './useItemDrag';
+import { DragItem } from './DragItem';
+import { isHidden } from './utils/isHidden';
 
 interface ColumnProps {
   text: string
   index: number
   id: string
+  isPreview?: boolean
 }
 
-export const Column = ({ text, index, id }: ColumnProps) => {
+export const Column = ({ text, index, id, isPreview }: ColumnProps) => {
   const { state, dispatch } = useAppState();
+  const ref = useRef<HTMLDivElement>(null);
+  const [, drop] = useDrop({
+    accept: 'COLUMN',
+    hover(item: DragItem) {
+      if (item.type === 'COLUMN') {
+        const dragIndex = item.index
+        const hoverIndex = index
+
+        if (dragIndex === hoverIndex) {
+          return
+        }
+
+        dispatch({ type: 'MOVE_LIST', payload: { dragIndex, hoverIndex } })
+        item.index = hoverIndex
+      }
+    }
+  });
+
+  const { drag } = useItemDrag({ type: 'COLUMN', id, index, text });
+
+  drag(drop(ref))
 
   return (
-    <ColumnContainer>
+    <ColumnContainer
+      isPreview={isPreview}
+      ref={ref}
+      isHidden={isHidden(isPreview, state.draggedItem, 'COLUMN', id)}
+    >
       <ColumnTitle>{text}</ColumnTitle>
       {state.lists[index].tasks.map((task, i) => (
         <Card text={task.text} key={task.id} index={i} />
       ))}
       <AddNewItem
-        toggleButtonText='+ Add another task'
-        onAdd={text => dispatch({ type: 'ADD_TASK', payload: { text, taskId: id } })}
+        toggleButtonText='+ Add another card'
+        onAdd={text =>
+          dispatch({ type: 'ADD_TASK', payload: { text, columnId: id } })
+        }
         dark
       />
     </ColumnContainer>
